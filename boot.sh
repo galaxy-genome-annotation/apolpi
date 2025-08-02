723048108b75cddfb28a57072e71e2bc753babe9
Bjoern Gruening <bjoern.gruening@gmail.com>
2025-08-02 11:52:48 +0200

    add a GUNICORN_TIMEOUT

--- boot.sh
+++ boot.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-exec gunicorn -b :80 --access-logfile - --error-logfile - apolpi:app
+exec gunicorn -b :80 --access-logfile - --error-logfile - --timeout ${GUNICORN_TIMEOUT:-30} apolpi:app
